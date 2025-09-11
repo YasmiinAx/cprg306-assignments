2d642b227bd04f79631275044df2ff74d3999b58
YasmiinAx <YasmiinAbdi535@gmail.com>
2025-09-11 14:00:58 -0600

    fixes

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ export default function StudentInfo() {
     return(
     <main>
         <h1>Yasmiin Abdullahi</h1>
-        <Link href="https://https://github.com/YasmiinAx">
+        <Link href="https://github.com/YasmiinAx">
         https://github.com/YasmiinAx
         </Link>
     </main>
